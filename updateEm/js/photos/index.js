3299500c2387fa95746bdfa5ca645f278c287323
Aaron Filson <aaronfilson@gmail.com>
2016-03-17 13:29:47 -0700

    Added test for log out function.

--- updateEm/js/photos/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-
-  require('./controllers/photos_controller')(app);
-
-  require('./directives/photo_display_directive')(app);
-  require('./directives/photo_form_directive')(app);//points to html template
-};
--- updateEm/test/test_entry.js
+++
@@ -1,4 +0,0 @@
-require(__dirname + '/../js/client.js');
-require("angular-mocks");
-
-require(__dirname + '/auth_controllers_test');
