7378cfe8785a3d2aa8e91d8dcf255c7b5b2addf9
Aaron Filson <aaronfilson@gmail.com>
2016-03-16 20:52:38 -0700

    Working on contents routes.

--- server/routes/content_routes.js
+++ server/routes/content_routes.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const jsonParser = require('body-parser').json();
 const Content = require(__dirname + '/../models/content');
@@ -7,13 +6,13 @@ const jwtAuth = require(__dirname + '/../lib/jwt_auth');
 
 var contentRouter = module.exports = exports = express.Router();
 
-contentRouter.get('/getlatest', (req, res) => {
-  Content.find({}, (err, data) => {
-    if (err) return handleDBError(err, res);
-
-    res.status(200).json(data);
-  });
-});
+// contentRouter.get('/getlatest', (req, res) => {
+//   Content.find({}, (err, data) => {
+//     if (err) return handleDBError(err, res);
+//
+//     res.status(200).json(data);
+//   });
+// });
 
 contentRouter.get('/getall', jwtAuth, (req, res) => {
   Content.find({contentId: req.user.id}, (err, data) => {
@@ -21,11 +20,10 @@ contentRouter.get('/getall', jwtAuth, (req, res) => {
 
     res.status(200).json(data);
   });
-
 });
 
 contentRouter.post('/newcontent', jwtAuth, jsonParser, (req, res) => {
-
+  debugger;
   var newContent = new Content(req.body);
   newContent.user_id = req.user.id;
   newContent.content = req.body.content;
@@ -41,16 +39,6 @@ contentRouter.post('/save', jwtAuth, jsonParser, (req, res) => {
   var newContent = new Content(req.body);
   newContent.user_id = req.user.id;
   newContent.save((err, data) => {
-    if (err) return handleDBError(err, res);
-
-    res.status(200).json(data);
-  });
-});
-
-contentRouter.put('/update', jwtAuth, jsonParser, (req, res) => {
-  var updateContent = new Content(req.body);
-  updateContent.user_id = req.user.id;
-  updateContent.save((err, data) => {
     if (err) return handleDBError(err, res);
 
     res.status(200).json(data);
