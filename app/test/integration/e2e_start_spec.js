40f13c996aa150f0b089668f12c67a5aea5828d2
Aaron Filson <aaronfilson@gmail.com>
2016-11-15 09:56:28 -0800

    Starting work on client integration tests.

---
+++ app/test/integration/e2e_start_spec.js
@@ -0,0 +1,8 @@
+/*eslint-disable*/
+describe('Protractor Demo App', function() {
+  it('should have a title', function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
+});
