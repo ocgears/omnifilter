220cdb04400330a7502354b9e6474b2cd3c4691b
Aaron Filson <aaronfilson@gmail.com>
2016-12-08 16:24:46 -0800

    Adding a test for bad input on the signin controller.

--- app/test/signin_controller_test.js
+++ app/test/signin_controller_test.js
@@ -55,5 +55,23 @@ describe('sign in controller basic', () => {
       expect($scope.email).toBe('tester');
       expect($window.localStorage.token).toBe('tokenstring');
     });
+
+    it('should have an error on bad input', () => {
+      // sending nothing, should get error from function
+      $scope.email = 'Bad Email : No at !';
+      $window.localStorage.token = 'Testing';
+      $scope.updateEmail = function() {
+        // this should not be called, as the error causes a return before it
+        $scope.email = null;
+        $window.localStorage.token = 'handle Error';
+      };
+      var errMsg = 'No user email!';
+      $httpBackend.expectGET('http://localhost:3000/signin')
+      .respond(400, errMsg);
+      $scope.submit('bad input');
+      $httpBackend.flush();
+      expect($scope.email).toBe('Bad Email : No at !');
+      expect($window.localStorage.token).toBe('Testing');
+    });
   });
 });
