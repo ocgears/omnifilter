543ae5a99c5187d77c1b62bf80b8eab2b3f40529
Aaron Filson <aaronfilson@gmail.com>
2016-12-14 19:10:55 -0800

    Changed background to a smaller image to improve load time. Added a basic protractor test.

--- app/test/integration/config.js
+++ app/test/integration/config.js
@@ -1,12 +1,12 @@
 module.exports = exports = {
   config: {
-    // seleniumAddress: 'http://localhost:4444/wd/hub',
-    directConnect: true,
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    // directConnect: true,
     framework: 'jasmine',
-    specs: ['e2e_start_spec.js']
-    // ,
-    // onPrepare: function() {
-    //   require('babel-core/register');
-    // }
+    specs: ['basic_ui_test.js'],
+    multiCapabilities:
+      [ { browserName: 'firefox' },
+      { browserName: 'chrome'
+    }]
   }
 };
